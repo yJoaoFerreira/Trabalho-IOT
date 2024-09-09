a967a1bb489de9e35320217dc53d754fef81a0c5
João Ferreira <yFerreiraJoao@gmail.com>
2024-09-09 16:36:41 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -4,5 +4,6 @@ void setup() { // coloque seu código setup aqui, para executar uma vez:
 }
 
 void loop() { // coloque seu código main aqui, para executar repetidamente:
+    delay(1000);
     Serial.println("tchau");
 }
